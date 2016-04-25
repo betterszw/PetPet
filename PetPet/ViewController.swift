afff8facb52b5da9111dd3ac50f845e4fd7d6d50
sunzw <sunzzww@icloud.com>
2016-04-25 16:15:07 +0800

    Initial Commit

---
+++ PetPet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PetPet
+//
+//  Created by 孙志伟 on 16/4/25.
+//  Copyright © 2016年 孙志伟. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
